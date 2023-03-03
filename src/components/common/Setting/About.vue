9bd213dd29f9c032d83b345f5182b8cc7df75c04
彭鞘 <878439818@qq.com>
2023-03-03 14:52:03 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -39,7 +39,7 @@ onMounted(() => {
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
+          CQDNSGPT项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
             href="https://github.com/killd/CQDNS-ChatGPT-Web"
