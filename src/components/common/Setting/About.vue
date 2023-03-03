e923f5e5b7d5e511d84f6a9d3cf0aa819e2df6e4
彭鞘 <878439818@qq.com>
2023-03-03 13:43:36 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -42,7 +42,7 @@ onMounted(() => {
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/killd/CQDNS-ChatGPT-Web"
             target="_blank"
           >
             Github
@@ -50,7 +50,6 @@ onMounted(() => {
           ，免费且基于 MIT 协议，没有任何形式的付费行为！
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
